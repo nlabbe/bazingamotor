e01a8ccc165881c46a55c74a8361bb93afc9b04b
Nicolás Labbé <nlabbedev@gmail.com>
2018-01-02 00:39:39 -0300

    Add override to ToString method.

--- Models/AbstractEngine.cs
+++ Models/AbstractEngine.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 namespace BazingaMotors.Models
 {
-    public class AbstractEngine
+    public class AbstractEngine : IEngine
     {
-        public AbstractEngine()
+        private int size;
+        private bool turbo;
+
+        public AbstractEngine(int size, bool turbo)
         {
+            this.size = size;
+            this.turbo = turbo;
+        }
+
+        public virtual int Size
+        {
+            get
+            {
+                return size;
+
+            }
+        }
+
+        public virtual bool Turbo
+        {
+            get
+            {
+                return turbo;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[AbstractEngine: Size={0}, Turbo={1}]", Size, Turbo);
         }
     }
 }
