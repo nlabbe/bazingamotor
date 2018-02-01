6888703c1ff6ccc9c9f4070f5bb299ac4afdaf8f
Nicolás Labbé <nlabbedev@gmail.com>
2018-02-01 16:58:15 -0300

    Add factory methos design pattern.

---
+++ Models/MicroFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BazingaMotors.Models
+{
+    public class MicroFactory : VehicleFactory
+    {
+        protected internal override IVehicle SelectVehicle(DrivingStyle style)
+        {
+            if (style == DrivingStyle.Economical)
+            {
+                return new Ebus(new StandardEngine(200, false));
+
+            }
+
+            return new Ebus(new StandardEngine(0, false));
+        }
+        
+        public MicroFactory()
+        {
+        }
+    }
+}
---
+++ Models/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BazingaMotors.Models
+{
+    public abstract class VehicleFactory
+    {
+        public enum DrivingStyle
+        {
+            Economical, Midrange, Large, Ecological 
+        }
+
+        public virtual IVehicle Build(DrivingStyle style, VehicleColour colour)
+        {
+            IVehicle v = SelectVehicle(style);
+            v.Paint(colour);
+            return v;
+            
+        }
+
+        //Este es el factory method!!!
+        protected internal abstract IVehicle SelectVehicle(DrivingStyle style);
+
+        public VehicleFactory()
+        {
+        }
+    }
+}
