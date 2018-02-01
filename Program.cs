15c996b64d3728d5f06a9f6f7b8e1d48a3c6b196
Nicolás Labbé <nlabbedev@gmail.com>
2018-02-01 17:23:10 -0300

    Build.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BazingaMotors.Models;
 
 namespace BazingaMotors
 {
@@ -6,7 +7,18 @@ namespace BazingaMotors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, welcome to Bazinga Motor Company");
+            Console.WriteLine("I want a economical car, coloured blue");
+
+
+
+            VehicleFactory busFactory = new MicroFactory();
+            IVehicle bus = busFactory.Build(VehicleFactory.DrivingStyle.Economical, VehicleColour.Blue);
+
+            Console.WriteLine(bus);
+
+            Console.Read();
+
         }
     }
 }
